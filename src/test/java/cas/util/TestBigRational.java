30a5273188ec14823102369d58e3199681c7f95e
eukaryote <minerguy31@gmail.com>
2017-05-12 20:58:34 -0600

    More tests

--- src/test/java/cas/util/TestBigRational.java
+++ src/test/java/cas/util/TestBigRational.java
@@ -11,7 +11,7 @@ public class TestBigRational {
 		// 1/2 + 1/3 = 5/6
 		assertEquals(BigRational.build("5/6"), BigRational.build("1/2").add(BigRational.build("1/3")));
 
-		// 1/2 + 1/2 = 1
+		// 1/2 + 1/2 = 1/1
 		assertEquals(BigRational.build("1/1"), BigRational.build("1/2").add(BigRational.build("1/2")));
 
 		BigRational b = BigRational.build("1/9");
@@ -47,5 +47,42 @@ public class TestBigRational {
 		assertEquals(BigRational.build("1/1"), BigRational.build("-1/1").divide(BigRational.build("-1/1")));
 		
 		assertEquals(BigRational.build("368956154146104041338487302204230/6842970045940769278989794312076920539427909963"), BigRational.build("12365452351254123654/-8706747200397588249059740289357").divide(BigRational.build("785938753984759/-29837659283745")));
+	}
+	
+	@Test
+	public void testIsZero() {
+		// common sense
+		assertTrue(BigRational.build(0).isZero());
+		assertFalse(BigRational.build(1).isZero());
+		
+		// rollover
+		assertFalse(BigRational.build(Integer.MAX_VALUE).isZero());
+		assertFalse(BigRational.build(1L + (long) Integer.MAX_VALUE).isZero());
+		
+		// negative zero
+		assertTrue(BigRational.build("-0").isZero());
+	}
+	
+	@Test
+	public void testIsPositive() {
+		// zero is not positive
+		assertFalse(BigRational.build("0").isPositive());
+
+		assertFalse(BigRational.build(-100).isPositive());
+		assertTrue(BigRational.build(100).isPositive());
+	}
+	
+	@Test
+	public void testIsNegative() {
+		// zero is not negative
+		assertFalse(BigRational.build("0").isNegative());
+
+		assertFalse(BigRational.build(100).isNegative());
+		assertTrue(BigRational.build(-100).isNegative());
+	}
+	
+	@Test
+	public void testReciprocal() {
+		assertEquals(BigRational.build("9/7"), BigRational.build("7/9").reciprocal());
 	}
 }
